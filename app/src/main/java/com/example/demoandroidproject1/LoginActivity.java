337205f4b2c5624dba1c99135e225b3b0686ed44
Hoshali Malhotra <hoshalimalhotra@192.168.1.8>
2023-01-23 01:00:59 +0530

    initial commit

---
+++ app/src/main/java/com/example/demoandroidproject1/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.demoandroidproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void hideHorizontalDivider(View view)
+    {
+        EditText editText=(EditText)view;
+        String s= editText.getHint().toString();
+        if(s.equals(R.string.email_hint)){
+            View v1=findViewById(R.id.dv_email);
+            v1.setVisibility(View.INVISIBLE);
+        }
+    }
+}
