6aed663711271de7f1140e4c983e3e7b70727877
Hoshali Malhotra <hoshalimalhotra@Hoshalis-MacBook-Pro.local>
2023-01-24 16:22:01 +0530

    API integration in user register and user login

--- app/src/main/java/com/example/demoandroidproject1/UserLoginActivity.java
+++ app/src/main/java/com/example/demoandroidproject1/UserLoginActivity.java
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -23,8 +34,40 @@ public class UserLoginActivity extends AppCompatActivity {
             v1.setVisibility(View.INVISIBLE);
         }
     }
+    public void userLogin(View view)
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String email=((EditText)findViewById(R.id.ev_log_em)).getText().toString();
+        String password=((EditText)findViewById(R.id.ev_log_ps)).getText().toString();
+        String url = "http://10.20.3.153:8769/user/signIn";
+        JSONObject jsonObject=new JSONObject();
+        try {
+            jsonObject.put("email", email);
+            jsonObject.put("password", password);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest( Request.Method.POST, url,jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("error",error.getMessage());
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+    }
     public void userSignUp(View view)
     {
+
         Intent intent=new Intent(this,RegisterActivity.class);
         startActivity(intent);
     }
